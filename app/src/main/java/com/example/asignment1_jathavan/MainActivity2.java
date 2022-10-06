a113956de396d5ad6b82388e227a499bacae8345
Jatha <Jatha@LAPTOP-FP7Q1G6B>
2022-10-06 12:51:13 -0400

    final

---
+++ app/src/main/java/com/example/asignment1_jathavan/MainActivity2.java
@@ -0,0 +1,68 @@
+package com.example.asignment1_jathavan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity
+{
+    //initialize variables
+    Button button;
+    TextView nameTextView, addressTextView,phoneTextView,emailTextView,toppingTextView,sizeTextView,xCheeseTextView,deliveryTextView,costTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        String name = getIntent().getStringExtra("keyName");
+        String address = getIntent().getStringExtra("keyAddress");
+        String number = getIntent().getStringExtra("keyNumber");
+        String email = getIntent().getStringExtra("keyEmail");
+        String topping = getIntent().getStringExtra("keyTopping");
+        String size = getIntent().getStringExtra("keySize");
+        String extraCheese = getIntent().getStringExtra("keyExtraCheese");
+        String delivery = getIntent().getStringExtra("keyDelivery");
+        String cost = getIntent().getStringExtra("keyCost");
+
+        nameTextView = findViewById(R.id.nameText);
+        addressTextView = findViewById(R.id.addressText);
+        phoneTextView = findViewById(R.id.phoneText);
+        emailTextView = findViewById(R.id.emailText);
+        toppingTextView = findViewById(R.id.toppingSelected);
+        sizeTextView = findViewById(R.id.sizeOfPizza);
+        xCheeseTextView = findViewById(R.id.extraCheese);
+        deliveryTextView = findViewById(R.id.deliveryCharge);
+        costTextView = findViewById(R.id.totalCost);
+
+        //display confirmation page
+        nameTextView.setText(name);
+        addressTextView.setText(address);
+        phoneTextView.setText(number);
+        emailTextView.setText(email);
+        toppingTextView.setText(topping);
+        sizeTextView.setText(size);
+        xCheeseTextView.setText(extraCheese);
+        deliveryTextView.setText(delivery);
+        costTextView.setText(cost);
+
+        //button to reorder pizza
+        button = findViewById(R.id.backButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //used to switch between activities
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
